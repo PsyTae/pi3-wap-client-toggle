0864a47fc4dd4f72aa42be784385fc660c47e6fd
Shaun Branham <shaun.branham@digitalallyinc.com>
2018-01-22 16:36:31 -0600

    test chagnes

--- src/test.js
+++ src/test.js
@@ -1,4 +1,11 @@
 const Iface = require("./index");
 
-const wlan1 = new Iface("WLAN1");
+const wlan1 = new Iface(
+  "WLAN1",
+  {
+    address: "192.168.254.0",
+    mask: "255.255.0.0"
+  },
+  { ssid: "dai", password: "HQWireless!" }
+);
 const wlan0 = new Iface("WLAN0");
