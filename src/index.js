ed97408928618bdd156262e241539e13c79b339a
Shaun Branham <shaun.branham@digitalallyinc.com>
2018-01-22 16:19:18 -0600

    console.log outs

--- src/index.js
+++ src/index.js
@@ -18,7 +18,8 @@ function Iface(iface, apConfig, clientConfig) {
     password: null
   };
   console.log("IFACE:", this.iface);
-  console.dir(this.config, { depth: null, colors: true });
+  console.dir(this.apConfig, { depth: null, colors: true });
+  console.dir(this.clientConfig, { depth: null, colors: true });
 }
 
 Iface.prototype.getSubNet = (address, mask) => {
