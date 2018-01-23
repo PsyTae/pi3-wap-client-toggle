89a8b646e9b323d5ed49c9b9b31cd635c8e835ae
Shaun Branham <shaun.branham@digitalallyinc.com>
2018-01-23 15:03:54 -0600

    constructor configuration

--- src/index.js
+++ src/index.js
@@ -5,26 +5,28 @@ const ip = require("ip");
 const os = require("os");
 
 function Iface(iface, clientConfig, apConfig) {
-  this.iface = iface ? iface.toLowerCase() : "wlan1";
-  this.apConfig = apConfig || {
-    address: "192.168.254.0",
-    dhcpPoolSize: 10,
-    subnetMask: "255.255.255.0"
-  };
-  this.apConfig.dhcp = false;
-  this.apConfig.mac = this.getMacAddress(this.iface);
-  this.apConfig.subnet = this.getSubNet(this.apConfig.address, this.apConfig.subnetMask);
-  this.apConfig.dhcpFirst = ip.fromLong(ip.toLong(this.apConfig.subnet.networkAddress) + 10);
-  this.apConfig.dhcpLast = ip.fromLong(ip.toLong(this.apConfig.subnet.networkAddress) + 10 + this.apConfig.dhcpPoolSize);
-  this.apConfig.dhcpLease = "12h";
-  this.apConfig.WAPssid = os.hostname();
-  this.apConfig.WAPpass = "0123456789";
+  if (this instanceof Iface) {
+    this.iface = iface ? iface.toLowerCase() : "wlan1";
+    this.apConfig = apConfig || {
+      address: "192.168.254.0",
+      dhcpPoolSize: 10,
+      subnetMask: "255.255.255.0"
+    };
+    this.apConfig.dhcp = false;
+    this.apConfig.mac = this.getMacAddress(this.iface);
+    this.apConfig.subnet = this.getSubNet(this.apConfig.address, this.apConfig.subnetMask);
+    this.apConfig.dhcpFirst = ip.fromLong(ip.toLong(this.apConfig.subnet.networkAddress) + 10);
+    this.apConfig.dhcpLast = ip.fromLong(ip.toLong(this.apConfig.subnet.networkAddress) + 10 + this.apConfig.dhcpPoolSize);
+    this.apConfig.dhcpLease = "12h";
+    this.apConfig.WAPssid = os.hostname();
+    this.apConfig.WAPpass = "0123456789";
 
-  this.clientConfig = clientConfig || {
-    ssid: null,
-    password: null
-  };
-  this.clientConfig.dhcp = true;
+    this.clientConfig = clientConfig || {
+      ssid: null,
+      password: null
+    };
+    this.clientConfig.dhcp = true;
+  } else return new Iface(iface, clientConfig, apConfig);
 }
 
 Iface.prototype.getSubNet = (address, mask) => {
