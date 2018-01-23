fe3c5ee3cb06ad271b9a7a1c00c69d24bd0bf544
Shaun Branham <shaun.branham@digitalallyinc.com>
2018-01-23 15:57:46 -0600

    updateing documentation

--- src/index.js
+++ src/index.js
@@ -3,7 +3,28 @@
 const child = require("child_process");
 const ip = require("ip");
 const os = require("os");
+/**
+ * @typedef {Object} clientConfig
+ * @property {string} ssid - SSID to look for and connect to
+ * @property {string} password - password for WIFI Connection
+ */
 
+/**
+ * @typedef {Object} apConfig
+ * @property {string} [address="192.168.254.0"] - Network IP Address for Access Point to use
+ * @property {string} [subnetMask="255.255.255.0"] - Subnet Mast for Access Point to use
+ * @property {number} [dhcpPoolSize=25] - Number of DHCP Clients that the Access Point will handle
+ * @property {string} [dhcpLease="12h"] - string representing how long the lease will be 123 = 123 seconds, 45m = 45 minutes, 12h = 12 hours, infinite = no expiration(not recomended)
+ * @property {string} [WAPssid="[HOSTNAME]"] - SSID to allow clients to connect to
+ * @property {string} [WAPpass="Pa$$w0rd"] - password to use to connect to SSID
+ */
+
+/**
+ * Iface Constructor
+ * @param {string} [iface="wlan1"] - interface name such as wlan0
+ * @param {clientConfig} clientConfig - Object used to Connect to Outside Netowrk
+ * @param {apConfig} apConfig - Object used to Establish a Wireless Access Point
+ */
 function Iface(iface, clientConfig, apConfig) {
   if (this instanceof Iface) {
     this.iface = iface ? iface.toLowerCase() : "wlan1";
@@ -13,7 +34,7 @@ function Iface(iface, clientConfig, apConfig) {
       dhcpLease: "12h",
       subnetMask: "255.255.255.0",
       WAPssid: os.hostname(),
-      WAPpass: "0123456789"
+      WAPpass: "Pa$$w0rd"
     };
     this.apConfig.dhcp = false;
     this.apConfig.mac = this.getMacAddress(this.iface);
