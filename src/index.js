5cbe8e0c4b246bf527c5ea5843dc46a804794304
Shaun Branham <shaun.branham@digitalallyinc.com>
2018-01-23 14:56:42 -0600

    adding in default ssid and passphase

--- src/index.js
+++ src/index.js
@@ -2,8 +2,9 @@
 
 const child = require("child_process");
 const ip = require("ip");
+const os = require("os");
 
-function Iface(iface, apConfig, clientConfig) {
+function Iface(iface, clientConfig, apConfig) {
   this.iface = iface ? iface.toLowerCase() : "wlan1";
   this.apConfig = apConfig || {
     address: "192.168.254.0",
@@ -12,12 +13,12 @@ function Iface(iface, apConfig, clientConfig) {
   };
   this.apConfig.dhcp = false;
   this.apConfig.mac = this.getMacAddress(this.iface);
-  this.apConfig.dhcpFirst;
-  this.apConfig.dhcpLast;
   this.apConfig.subnet = this.getSubNet(this.apConfig.address, this.apConfig.subnetMask);
-
   this.apConfig.dhcpFirst = ip.fromLong(ip.toLong(this.apConfig.subnet.networkAddress) + 10);
   this.apConfig.dhcpLast = ip.fromLong(ip.toLong(this.apConfig.subnet.networkAddress) + 10 + this.apConfig.dhcpPoolSize);
+  this.apConfig.dhcpLease = "12h";
+  this.apConfig.WAPssid = os.hostname();
+  this.apConfig.WAPpass = "0123456789";
 
   this.clientConfig = clientConfig || {
     ssid: null,
