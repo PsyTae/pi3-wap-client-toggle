0dca42139de5da90d4303a7c2a0b2f07c5c1d1be
Shaun Branham <shaun.branham@digitalallyinc.com>
2018-02-01 11:13:03 -0600

    testing

--- src/index.js
+++ src/index.js
@@ -75,40 +75,34 @@ function NetSet() {
    * @param {netConfig} netConfig - Object used to store all connection info for network setup
    */
   const initNetwork = (startAsHotspot, netConfig) => {
+    const objKeys = Object.keys(obj);
+    const configKeys = netConfig ? Object.keys(netConfig) : [];
     obj.actingAsHotSpot = startAsHotspot ? !!startAsHotspot : true;
 
-    obj.eth0 = netConfig && netConfig.eth0 ? netConfig.eth0 : {};
-    obj.static = netConfig && netConfig.static ? netConfig.static : [];
-    obj.wlan0 = netConfig && netConfig.wlan0 ? netConfig.wlan0 : {};
-
-    obj.eth0.server = netConfig && netConfig.eth0 && netConfig.eth0.server ? netConfig.eth0.server : {};
-    obj.wlan0.client = netConfig && netConfig.wlan0 && netConfig.wlan0.client ? netConfig.wlan0.client : {};
-    obj.wlan0.server = netConfig && netConfig.wlan0 && netConfig.wlan0.server ? netConfig.wlan0.server : {};
-
-    obj.eth0.mac = netConfig && netConfig.eth0 && netConfig.eth0.mac ? netConfig.eth0.mac : null;
-    obj.wlan0.mac = netConfig && netConfig.wlan0 && netConfig.wlan0.mac ? netConfig.wlan0.mac : null;
+    objKeys.diff(configKeys).forEach(elem => {
+      objKeys.push(elem);
+      obj[elem] = elem === "static" ? netConfig[elem] : {};
+      if (elem !== "static") obj[elem].mac = netConfig[elem].mac ? netConfig[elem].mac : null;
+      if (netConfig[elem].server) {
+        obj[elem].server = {};
+        obj[elem].server.address = netConfig[elem].server.address ? netConfig[elem].server.address : "10.255.255.255";
+        obj[elem].server.dhcpFirst = netConfig[elem].server.dhcpFirst ? netConfig[elem].server.dhcpFirst : null;
+        obj[elem].server.dhcpLast = netConfig[elem].server.dhcpLast ? netConfig[elem].server.dhcpLast : null;
+        obj[elem].server.dhcpLease = netConfig[elem].server.dhcpLease ? netConfig[elem].server.dhcpLease : "12h";
+        obj[elem].server.dhcpPoolSize = netConfig[elem].server.dhcpPoolSize ? netConfig[elem].server.dhcpPoolSize : 10;
+        obj[elem].server.subnet = netConfig[elem].server.subnet ? netConfig[elem].server.subnet : null;
+        obj[elem].server.subnetMask = netConfig[elem].server.subnetMask ? netConfig[elem].server.subnetMask : "255.255.255.0";
+      }
+      if (netConfig[elem].client) {
+        obj[elem].client = {};
+        obj[elem].client.pass = netConfig[elem].client.pass ? netConfig[elem].client.pass : "Pa$$w0rd";
+        obj[elem].client.ssid = netConfig[elem].client.ssid ? netConfig[elem].client.ssid : `VL${os.hostname().toUpperCase()}`;
+      }
+    });
 
     obj.eth0.server.address = netConfig && netConfig.eth0 && netConfig.eth0.server.address ? netConfig.eth0.server.address : "10.0.0.1";
-    obj.eth0.server.dhcpFirst = netConfig && netConfig.eth0 && netConfig.eth0.server.dhcpFirst ? netConfig.eth0.server.dhcpFirst : null;
-    obj.eth0.server.dhcpLast = netConfig && netConfig.eth0 && netConfig.eth0.server.dhcpLast ? netConfig.eth0.server.dhcpLast : null;
-    obj.eth0.server.dhcpLease = netConfig && netConfig.eth0 && netConfig.eth0.server.dhcpLease ? netConfig.eth0.server.dhcpLease : "12h";
-    obj.eth0.server.dhcpPoolSize = netConfig && netConfig.eth0 && netConfig.eth0.server.dhcpPoolSize ? netConfig.eth0.server.dhcpPoolSize : 10;
-    obj.eth0.server.subnet = netConfig && netConfig.eth0 && netConfig.eth0.server.subnet ? netConfig.eth0.server.subnet : null;
-    obj.eth0.server.subnetMask = netConfig && netConfig.eth0 && netConfig.eth0.server.subnetMask ? netConfig.eth0.server.subnetMask : "255.255.255.0";
+    obj.wlan0.server.address = netConfig && netConfig.wlan0 && netConfig.wlan0.server.address ? netConfig.wlan0.server.address : "10.10.10.1";
 
-    obj.wlan0.client.pass = netConfig && netConfig.wlan0 && netConfig.wlan0.client.pass ? netConfig.wlan0.client.pass : "Pa$$w0rd";
-    obj.wlan0.client.ssid = netConfig && netConfig.wlan0 && netConfig.wlan0.client.ssid ? netConfig.wlan0.client.ssid : `VL${os.hostname().toUpperCase()}`;
-
-    obj.wlan0.server.address = netConfig && netConfig.wlan0 && netConfig.wlan0.server.address ? netConfig.wlan0.server.address : "10.0.0.1";
-    obj.wlan0.server.dhcpFirst = netConfig && netConfig.wlan0 && netConfig.wlan0.server.dhcpFirst ? netConfig.wlan0.server.dhcpFirst : null;
-    obj.wlan0.server.dhcpLast = netConfig && netConfig.wlan0 && netConfig.wlan0.server.dhcpLast ? netConfig.wlan0.server.dhcpLast : null;
-    obj.wlan0.server.dhcpLease = netConfig && netConfig.wlan0 && netConfig.wlan0.server.dhcpLease ? netConfig.wlan0.server.dhcpLease : "12h";
-    obj.wlan0.server.dhcpPoolSize = netConfig && netConfig.wlan0 && netConfig.wlan0.server.dhcpPoolSize ? netConfig.wlan0.server.dhcpPoolSize : 10;
-    obj.wlan0.server.subnet = netConfig && netConfig.wlan0 && netConfig.wlan0.server.subnet ? netConfig.wlan0.server.subnet : null;
-    obj.wlan0.server.subnetMask = netConfig && netConfig.wlan0 && netConfig.wlan0.server.subnetMask ? netConfig.wlan0.server.subnetMask : "255.255.255.0";
-
-    const objKeys = Object.keys(obj);
-    objKeys.splice(objKeys.indexOf("actingAsHotSpot"), 1);
     objKeys.splice(objKeys.indexOf("static"), 1);
 
     objKeys.forEach(elem => {
@@ -135,5 +129,7 @@ function NetSet() {
 
   return publicAPI;
 }
+
+Array.prototype.diff = a => this.filter(i => a.indexOf(i) < 0);
 
 module.exports = NetSet;
