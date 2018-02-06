e0f2cfcfea2e778a695cb05c7f44dfee3fd0691a
Shaun Branham <shaun.branham@digitalallyinc.com>
2018-02-06 16:01:27 -0600

    babel build

--- dist/index.js
+++ dist/index.js
@@ -4,6 +4,10 @@ var _child_process = require("child_process");
 
 var _child_process2 = _interopRequireDefault(_child_process);
 
+var _fs = require("fs");
+
+var _fs2 = _interopRequireDefault(_fs);
+
 var _ip = require("ip");
 
 var _ip2 = _interopRequireDefault(_ip);
@@ -12,47 +16,73 @@ var _os = require("os");
 
 var _os2 = _interopRequireDefault(_os);
 
-var _util = require("util");
-
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
 /**
  * Object to initialize our available network interfaces with
- * @typedef {Object} netConfig
+ * @typedef {Object} obj
  * @property {boolean} [actingAsHotSpot=true] - Whether to start WLAN0 as a hotspot or not.
- * @property {static[]} [static=[]] - array of network devices that need to have a static ip address reserved for them, defaults to empty array
+ * @property {static} [static=[]] - array of network devices that need to have a static ip address reserved for them, defaults to empty array
  * @property {...interface} [interface] - eth0, wlan0, wlan1, will contain client/server object, or both for the interface in question
  */
 
 /**
- * @typedef {Object} static
+ * Object passed in from calling program to initialize our network setup.
+ * @typedef {Object} netConfig
+ * @property {static} [static=[]] - array of network devices that need to have a static ip address reserved for them, defaults to empty array
+ * @property {...interface} [interface] - eth0, wlan0, wlan1, will contain client/server object, or both for the interface in question
+ */
+
+/**
+ * Array of Network Devices that need to have a fixed IP address, defautls to an empty array
+ * @typedef {Object[]} static
  * @property {string} mac - mac address of network device needing a reserved IP address
  * @property {string} ipAddress - ip address to be reserved for network device
  * @property {string} name - name to give to device in dns
  */
 
 /**
- * eth0, wlan0, wlan1: will contain client/server object, or both for the interface in question
- * @typedef {Object} interface
- * @property {client[]} client - Object with Client Properties for the Interface
- * @property {server} server - Object with Server Properties for the Interface
- */
-
-/**
- * @typedef {Object} client
+ * List of APs to listen for and connect to when in WiFi mode.
+ * @typedef {Object[]} clients
  * @property {string} name - Nickname to give the wireless Access Point Connection
  * @property {string} pass - Passphase to use to connect to a Wireless Access Point
  * @property {string} ssid - SSID to use to connect to a Wireless Access Point
  */
 
 /**
+ * calculated by ip, needs an ip address, and subnetMask
+ * @typedef {Object} subnet
+ * @property {string} subnet.networkAddress - network address for the subnet
+ * @property {string} subnet.firstAddress - first useable ip address of the subnet
+ * @property {string} subnet.lastAddress - last usable ip address of the subnet
+ * @property {string} subnet.broadcastAddress - broadcast address for the subnet
+ * @property {string} subnet.subnetMask - subnetmask for the subnet
+ * @property {string} subnet.subnetMaskLength - cidr length for the subnet
+ * @property {string} subnet.numHosts - how many hosts found in th subnet
+ * @property {string} subnet.length - how many address are in the subnet, indluding network and broadcast
+ * @property {function} subnet.contains - function to see if an Ip Address given falls inside the specified subnet
+ */
+
+/**
  * @typedef {Object} server
+ * @property {string} address - Static IP Address to give interface when in DHCP Server Mode
+ * @property {string} [subnetMask="255.255.255.0"] - Subnet mask to use when in DHCP Server Mode
+ * @property {string} [dhcpLease="12h"] - How long the dhcp lease should be good for 1h = 1 hour, 1m = 1 minute, 30 = 30 seconds | defaults to 12h
+ * @property {number} [dhcpPoolSize=10] - Size of the DHCP Pool to lease from
+ * @property {subnet} subnet - Calculated based on address and subnetMask
+ * @property {string} dhcpFirst - Calculated based on Subnet size and dhcpPoolSize
+ * @property {string} dhcpLast - Calculated based on Subnet size and dhcpPoolSize
+ */
+
+/**
+ * eth0, wlan0, wlan1: will contain client/server object, or both for the interface in question
+ * @typedef {Object} interface
+ * @property {clients=} clients - Object with Client Properties for the Interface
+ * @property {server=} server - Object with Server Properties for the Interface
  */
 
 // npm i -D babel-cli babel-preset-env
 /* eslint consistent-return: 0, no-param-reassign: 0, no-use-before-define: ["error", { "functions": false }], no-else-return: 0, no-nested-ternary: 0, no-extend-native: 0 */
-
-var execFilePromise = (0, _util.promisify)(_child_process2.default.execFile);
 
 function NetSet() {
   var obj = {};
@@ -65,53 +95,77 @@ function NetSet() {
     return _ip2.default.subnet(ipAddress, subnet);
   };
 
-  var stopServices = function stopServices() {
-    return new Promise(function (stopResolve, stopReject) {
-      execFilePromise("systemctl", ["stop", "hostapd"]).then(execFilePromise("systemctl", ["stop", "dnsmasq"])).then(function () {
-        return stopResolve();
-      }).catch(function (error) {
-        return stopReject(error);
+  var stopServices = function stopServices(callback) {
+    return new Promise(function (resolve, reject) {
+      var cbObj = {};
+      _child_process2.default.execFile("systemctl", ["stop", "hostapd"], function (hostErr, hostStdOut, hostStdErr) {
+        if (hostErr) return callback ? callback(hostErr) : reject(hostErr);
+        cbObj.hostapd = {
+          StdOut: hostStdOut,
+          StdErr: hostStdErr
+        };
+        _child_process2.default.execFile("systemctl", ["stop", "dnsmasq"], function (dnsErr, dnsStdOut, dnsStdErr) {
+          if (dnsErr) return callback ? callback(dnsErr) : reject(dnsErr);
+          cbObj.dnsmasq = {
+            StdOut: dnsStdOut,
+            StdErr: dnsStdErr
+          };
+          return callback ? callback(null, cbObj) : resolve(cbObj);
+        });
       });
     });
   };
 
-  var startServices = function startServices() {
-    return new Promise(function (startResolve, startReject) {
-      execFilePromise("systemctl", ["start", "dnsmasq"]).then(execFilePromise("systemctl", ["start", "hostapd"])).then(function () {
-        return startResolve();
-      }).catch(function (error) {
-        return startReject(error);
+  var startServices = function startServices(callback) {
+    return new Promise(function (resolve, reject) {
+      var cbObj = {};
+      _child_process2.default.execFile("systemctl", ["start", "hostapd"], function (hostErr, hostStdOut, hostStdErr) {
+        if (hostErr) return callback ? callback(hostErr) : reject(hostErr);
+        cbObj.hostapd = {
+          StdOut: hostStdOut,
+          StdErr: hostStdErr
+        };
+        _child_process2.default.execFile("systemctl", ["start", "dnsmasq"], function (dnsErr, dnsStdOut, dnsStdErr) {
+          if (dnsErr) return callback ? callback(dnsErr) : reject(dnsErr);
+          cbObj.dnsmasq = {
+            StdOut: dnsStdOut,
+            StdErr: dnsStdErr
+          };
+          return callback ? callback(null, cbObj) : resolve(cbObj);
+        });
       });
     });
   };
 
-  var setStates = function setStates(states) {
-    console.log(states);
-    console.dir(obj, { depth: null });
-    // obj.actingAsHotSpot = !state;
-    // if (state) {
-    // if obj.actingAsHotSpot === false needs to be flipped to true by end of if to signify acting as hotspot
-    // todo: check files to see if services need to be stopped and files need to be reconfigured
-    // todo: backup files if originals are not already saved
-    // todo: stop services
-    // todo: setup files for hostapd, and dnsmasq
-    // todo: start services
-    // todo: set obj.actingAsHotSpot = true
-    // } else {
-    // if obj.actingAsHotSpot === true needs to be flipped to false by end of if to signify actingg as client
-    // todo: check files to see if services need to be stopped and files need to be reconfigured
-    // todo: backup files if originals are not already saved
-    // todo: stop services
-    // todo: setup files to connect to wifi
-    // todo: start services
-    // todo: set obj.actingAsHotSpot = false
-    // }
+  var setStates = function setStates(states, callback) {
+    return new Promise(function (resolve, reject) {
+      console.log(states);
+      console.dir(obj, { depth: null });
+      states.forEach(function (iface) {});
+      return callback ? callback(null, obj) : resolve(obj);
+    });
   };
+
+  // if obj.actingAsHotSpot === false needs to be flipped to true by end of if to signify acting as hotspot
+  // todo: check files to see if services need to be stopped and files need to be reconfigured
+  // todo: backup files if originals are not already saved
+  // todo: stop services
+  // todo: setup files for hostapd, and dnsmasq
+  // todo: start services
+  // todo: set obj.actingAsHotSpot = true
+
+  // if obj.actingAsHotSpot === true needs to be flipped to false by end of if to signify actingg as client
+  // todo: check files to see if services need to be stopped and files need to be reconfigured
+  // todo: backup files if originals are not already saved
+  // todo: stop services
+  // todo: setup files to connect to wifi
+  // todo: start services
+  // todo: set obj.actingAsHotSpot = false
 
   /**
    * Initialize Network
-   * @param {boolean} [startAsHotspot=true] - Whether or not to start as hotspot or client
-   * @param {netConfig} netConfig - Object used to store all connection info for network setup
+   * @type {boolean} [startAsHotspot=true] - Whether or not to start as hotspot or client
+   * @type {netConfig} netConfig - Object used to store all connection info for network setup
    */
   var initNetwork = function initNetwork(startAsHotspot, netConfig) {
     var states = {};
@@ -193,6 +247,22 @@ function NetSet() {
 
   return publicAPI;
 }
+
+var files = ["/etc/default/hostapd", "/etc/dhcpd.conf", "/etc/dnsmasq.conf", "/etc/hostapd/hostapd.conf", "/etc/network/interfaces", "/etc/wpa_supplicant/wpa_supplicant.conf"];
+
+var makeBackup = function makeBackup(file) {
+  return new Promise(function (resolve, reject) {
+    resolve("file " + file + ".bak exists: " + _fs2.default.existsSync(file + ".bak"));
+  });
+};
+
+var mapBackups = files.map(makeBackup);
+
+var backupFiles = Promise.all(mapBackups);
+
+backupFiles.then(function (data) {
+  return console.log(data);
+});
 
 Array.prototype.diff = function (a) {
   return this.filter(function (i) {
